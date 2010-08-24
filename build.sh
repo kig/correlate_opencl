6e031e3c1312c949ef7d4925e1c34c3a67175339
kig <ilmari.heikkinen@gmail.com>
2010-08-24 16:54:06 +0300

    build.sh: parallel compilation

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 export CPLUS_INCLUDE_PATH=$ATISTREAMSDKROOT/include:$CPLUS_INCLUDE_PATH
 export LIBRARY_PATH=$ATISTREAMSDKROOT/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ATISTREAMSDKROOT/lib:$LD_LIBRARY_PATH
-g++ -O3 -msse3 -mfpmath=sse -fopenmp -lOpenCL -lm -o corr_500 corr_500.cpp
-g++ -O3 -msse3 -mfpmath=sse -fopenmp -lOpenCL -lm -o corr corr.cpp
-g++ -O3 -msse3 -mfpmath=sse -fopenmp -lm -lGLEW -lGL -lGLU -lglut -o corr_gl corr_gl.cpp
-g++ -O3 -msse3 -mfpmath=sse -fopenmp -lOpenCL -lm -o corr_naive corr_naive.cpp
+g++ -O3 -msse3 -mfpmath=sse -fopenmp -lOpenCL -lm -o corr_500 corr_500.cpp &
+g++ -O3 -msse3 -mfpmath=sse -fopenmp -lOpenCL -lm -o corr corr.cpp &
+g++ -O3 -msse3 -mfpmath=sse -fopenmp -lm -lGLEW -lGL -lGLU -lglut -o corr_gl corr_gl.cpp &
+g++ -O3 -msse3 -mfpmath=sse -fopenmp -lOpenCL -lm -o corr_naive corr_naive.cpp &
 g++ -O3 -lOpenCL -lm -o memsum memsum.cpp
